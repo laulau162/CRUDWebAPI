45190c2c42ce3f10e7be30a50cbcb4366b8e6e00
DAW205 <daw205@educantabria.es>
2023-03-04 12:11:10 +0100

    Agregar archivos de proyecto.

---
+++ Controllers/AlumnosController.cs
@@ -0,0 +1,69 @@
+﻿using CRUDWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace CRUDWebAPI.Controllers
+{
+    public class AlumnosController : ApiController
+    {
+        private C2BEntities1 db = new C2BEntities1();
+        [ResponseType(typeof(Alumno))]
+        public Alumno ObtenerEstudiante(int id)
+        {
+            return db.Alumnos.FirstOrDefault(e => e.Id == id);
+        }
+        [HttpGet]
+        public IEnumerable<Alumno> ObtenerEstudiantes()
+        {
+            return db.Alumnos;
+        }
+
+        
+       
+
+        [HttpPost]
+        public IHttpActionResult NuevoAlumno (Alumno a)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Datos Incorrectos!!!");
+            db.Alumnos.Add(a);
+            db.SaveChanges();
+            return Ok();
+
+        }
+
+        [HttpPut]
+        public IHttpActionResult ModificarAlumno(Alumno a)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Datos Incorrectos!!!");
+            var antiguo=db.Alumnos.FirstOrDefault(al=>al.Id==a.Id);
+            if(antiguo!=null)
+            {
+                antiguo.Nombre = a.Nombre;  
+                antiguo.Apellidos= a.Apellidos;
+                //antiguo = a;
+                db.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Borrar(int id)
+        {
+            db.Alumnos.Remove(db.Alumnos.FirstOrDefault(al => al.Id == id));
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/EstudiantesController.cs
@@ -0,0 +1,79 @@
+﻿using CRUDWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDWebAPI.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private C2BEntities1 db = new C2BEntities1();
+        // GET: Estudiantes
+         public ActionResult Index()
+        {
+            IEnumerable<Alumno> l = null;
+            using (var cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri("http://localhost:58908/api/");
+                var respuesta = cliente.GetAsync("Alumnos/ObtenerEstudiantes");
+                respuesta.Wait();
+
+                var resultado=respuesta.Result;
+
+                var lectura=resultado.Content.ReadAsAsync<IList<Alumno>>();
+                lectura.Wait();
+
+                l = lectura.Result;
+            }
+            return View(l);
+        }
+
+        public ActionResult Modificar(int id)
+        {
+            Alumno a = null;
+
+            return View(a);
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(Alumno a)
+        {
+            using (var cliente=new HttpClient())
+            {
+                cliente.BaseAddress = new Uri("http://localhost:58908/api/Alumnos/");
+                var alta = cliente.PostAsJsonAsync<Alumno>("NuevoAlumno", a);
+                alta.Wait();
+
+                var result = alta.Result;
+                if(result.IsSuccessStatusCode)
+                    return RedirectToAction("Index");   
+            }
+            ModelState.AddModelError(String.Empty, "Error en el Servidor, Contacta con tu Proveedor!!!");    
+            return View(a);
+        }
+
+       
+        public ActionResult Borrar(int id)
+        {
+            using (var cliente=new HttpClient())
+            {
+                cliente.BaseAddress= new Uri("http://localhost:58908/api/Alumnos/");
+                var r = cliente.DeleteAsync("/Borrar/" + id.ToString());
+                r.Wait();
+
+                var resultado=r.Result;
+                
+                return RedirectToAction("Index");
+                
+            }   
+        }
+
+    }
+}
